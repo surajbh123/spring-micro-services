2368ae93b122d04467290790b90e541cdab9a8b6
surajbh123 <surajbh123@github.com>
2021-11-02 20:21:41 +0530

    mutiple env config added

--- config-server/src/main/java/com/suraj/masr/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/suraj/masr/config/server/ConfigServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+//Centralise config Server
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
